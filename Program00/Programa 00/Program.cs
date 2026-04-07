5582cf34a10c892f517b9cc9d2ef1aa0b8323c7a
Nico <nicodaniel207@gmail.com>
2026-04-07 14:21:11 -0300

    Ejercicios

---
+++ Program00/Programa 00/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Programa_00;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ejercicio 00 - Hello, World!");
+
+        Console.WriteLine("Me llamo Nicolas Daniel");
+        Console.WriteLine("Tengo 24 años");
+        Console.WriteLine("Actualmente estoy estudiando la Tecnicatura Universitaria en Programacion");
+        Console.WriteLine("En la Univerdad Tecnologica Nacional");
+        Console.WriteLine("Me encuentro Cursando la materia de Programacion III");
+    }
+}
---
+++ Program02/Programa02/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Programa02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--- Año de Cursado ---");
+
+            // Declaración de un entero
+            int año = 2026;
+
+            Console.WriteLine("1. TIPO ENTERO (int):");
+            Console.WriteLine("   Se usa para representar fechas, años o contadores.");
+            Console.WriteLine("   Valor guardado (Año actual): " + año);
+
+            Console.WriteLine("\nPresiona ENTER para cerrar.");
+            Console.ReadLine();
+
+
+    }
+}
